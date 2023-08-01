83e335411ba71d700af8431e32b426cfa3c24967
Fabio <josefabiosan@gmail.com>
2023-07-31 22:15:32 -0300

    implementando tratamento de erro

--- lib/desafio_imc.dart
+++ lib/desafio_imc.dart
@@ -4,19 +4,23 @@ import 'models/tabela_classificacao.dart';
 
 void appImc() {
   print('Bora ver seu IMC');
-  var nome = lerConsole('Digite seu nome');
-  var peso = double.parse(lerConsole('Digite seu peso'));
-  var altura = double.parse(lerConsole('Digite sua altura'));
+  try {
+    var nome = lerConsole('Digite seu nome');
+    var peso = double.parse(lerConsole('Digite seu peso'));
+    var altura = double.parse(lerConsole('Digite sua altura'));
 
-  Pessoa pessoa = Pessoa(nome, peso, altura);
+    Pessoa pessoa = Pessoa(nome, peso, altura);
 
 //Variavel que armazena o metodo calcularIMC do objeto Pessoa
-  double resultado = pessoa.calcularIMC();
-  print('O IMC de ${pessoa.nome} é ${resultado.toStringAsFixed(2)}');
+    double resultado = pessoa.calcularIMC();
+    print('O IMC de ${pessoa.nome} é ${resultado.toStringAsFixed(2)}');
 
 //Variavel que armazena a funcao classificacao que recebe como parametro a variavel resultado
-  String frase = classificacao(resultado);
-  print(frase);
+    String frase = classificacao(resultado);
+    print(frase);
+  } catch (e) {
+    print("Tente novamente, digite a informação correta");
+  }
 }
 
 //criar classe pessoa OK

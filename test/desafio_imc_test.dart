ca9d36c7824e20573298e549f5b97d1820fb767c
Fabio <josefabiosan@gmail.com>
2023-08-06 15:54:26 -0300

    implementacao de testes

--- test/desafio_imc_test.dart
+++ test/desafio_imc_test.dart
@@ -2,8 +2,15 @@ import 'package:desafio_imc/models/pessoa.dart';
 import 'package:test/test.dart';
 
 void main() {
-  var pessoa = Pessoa('', 68.0, 1.75);
-  test('calcular imc', () {
-    expect(pessoa.calcularIMC(), 22.0);
+  final pessoa = Pessoa('', 68.0, 1.75);
+  test('Resultado do imc', () {
+    //variavael para armazenar o resultado esperado do tipo double
+    double expectedResult = 22.20;
+    //resultado do calculo da funcao calcularIMC e depois convertido em string para posterior comparacao.
+    expect(
+      pessoa.calcularIMC().toStringAsFixed(2),
+      //o resultado é convertido para tipo string com dois digitos apos a virgula
+      expectedResult.toStringAsFixed(2),
+    );
   });
 }
